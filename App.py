598ba31c81dd4b61c3402a36230af26d33762935
Miguel Escalona - DLSU <jose_miguel_escalona@dlsu.edu.ph>
2022-09-12 20:25:21 +0800

    Updated
    1. Added pydrive
    2. Updated Install.bat script to include pydrive
    2. Added function for uploading to Google Drive
    3. Updated TrackNRecord to return filename after completion

--- App.py
+++ App.py
@@ -1,7 +1,25 @@
-import tkinter as tk
+#Google Drive
+import pydrive #Install using pydrive
+
+#OS
+import os
+
+#Facial Tracker and Recorder
 import TrackNRecord as tr
+
+def Upload(filename):
+    print("Uploading to Google Drive...")
+    print("File selected: " + filename)
+    '''
+    Do Google Drive related code here
+    Suggested article: https://medium.com/analytics-vidhya/how-to-connect-google-drive-to-python-using-pydrive-9681b2a14f20
+    '''
 
 if __name__== "__main__":
     print("Launching app...")
-    tr.TrackNRecord()
+    os.system('cls')
+    filename = tr.TrackNRecord() #Resulting output filename of the app.
+    print("=================================================")
+    print("Output file saved as: " + filename)
+    Upload(filename)
     print("Exiting app...")
--- Install.bat
+++ Install.bat
@@ -1,3 +1,4 @@
 pip install pyaudio
 pip install serial
 pip install opencv-python
+pip install pydrive
