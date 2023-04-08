4b7cd8f0d2838ab21ac7db56921fbd31c9c8d501
Miguel Escalona - DLSU <71485171+jm55DLSU@users.noreply.github.com>
2023-04-08 21:08:49 +0800

    Updated to latest version

--- Blink Detect/Legacy/detect.py
+++ Blink Detect/Legacy/detect.py
@@ -54,7 +54,7 @@ class detect:
 		end = time.time()
 		time_diff = 0.0
 		capture_time = 2
-		#blink_limit = 3
+		blink_limit = 3
 
 		while self.ret:
 				# this will keep the web-cam running and capturing the image for every loop
@@ -71,31 +71,37 @@ class detect:
 					roi_face = gray[y:y + h, x:x + w] #face detector
 					roi_face_clr = image[y:y + h, x:x + w] #image
 					eyes = eye_cascade.detectMultiScale(roi_face, 1.1, 6, minSize=(50,50)) #list of eyes
-					cv2.putText(image, "Blinks: "+ str(blink_counter), (25,70), cv2.FONT_HERSHEY_SIMPLEX, .5, (1, 190, 200), 2)
+					if not first_read:
+						cv2.putText(image, "Blinks: "+ str(blink_counter), (25,70), cv2.FONT_HERSHEY_SIMPLEX, .5, (1, 190, 200), 2)
 					#cv2.putText(image, "Eye count:", str(len(eyes), "Eye coor:", *eyes, sep=" "), (10,395), cv2.FONT_HERSHEY_SIMPLEX, .5, (0,0,0), 2)
 						#Check list of eyes if more than 2, thus it sees eyes. Needs to be outside for-loop for better performance
 					if len(eyes) >= 2: #eyes open
 						start = time.time()
-						cv2.putText(image, "Eyes Open", (25,35), cv2.FONT_HERSHEY_SIMPLEX, .5, (255, 255, 255), 2)
-						for (ex,ey,ew,eh) in eyes:
-							cv2.rectangle(roi_face_clr,(ex,ey),(ex+ew,ey+eh),(255, 153, 255),2)
-						#print(time.time(),str(eyes[0][0])+","+str(eyes[0][1])+"," +str(eyes[0][2])+","+str(eyes[0][3])+","+str(eyes[1][0])+","+str(eyes[1][1])+","+str(eyes[1][2])+","+str(eyes[1][3						
+						#print(time.time(),str(eyes[0][0])+","+str(eyes[0][1])+"," +str(eyes[0][2])+","+str(eyes[0][3])+","+str(eyes[1][0])+","+str(eyes[1][1])+","+str(eyes[1][2])+","+str(eyes[1][3]))
 					else: #eyes closed
 						end = time.time()
 						time_diff = end - start
-						if time_diff > capture_time: #Will trigger only if 5s has been reached
-							cv2.putText(image, "Long Blink Detected! (Eyes closed at " + str(round(time_diff,2)) + "s)", (25,35), cv2.FONT_HERSHEY_SIMPLEX, .5, (0, 0, 0), 2)
-							blink_frame = image #Make a copy of the frame at blink capture so that it won't move with the continous camera feeds
-							start = time.time() #Reset time to current time
-							blink_counter = blink_counter + 1
-							print("Blink detected!")
-							self.state = blink_counter
-							cv2.putText(image, "Long Blinks Detected: " + str(blink_counter), (440,70), cv2.FONT_HERSHEY_SIMPLEX, .5, (0, 0, 0), 2)
-							cv2.putText(image, time.ctime(end), (10,460), cv2.FONT_HERSHEY_SIMPLEX, .5, (0, 0, 0), 2)									#print("Image saved: ", cv2.imwrite(str(time.ctime(end).replace(":","").replace(" ",""))+".jpg",blink_frame))
-							#cv2.imshow("Captured Blink", blink_frame) #Show captured frame
 						cv2.putText(image, "Eye/s Closed @" + str(round(time_diff,2)) + "s", (460,35), cv2.FONT_HERSHEY_SIMPLEX, .5, (0, 0, 0), 2)
 						#print("0,0,0,0,0,0,0,0")
-						#Plotting and GUI & output
+						#Plotting and GUI output
+					for (ex,ey,ew,eh) in eyes:
+						cv2.rectangle(roi_face_clr,(ex,ey),(ex+ew,ey+eh),(255, 153, 255),2)
+						if len(eyes) >= 2: #eyes found
+							if first_read:
+								cv2.putText(image, "Eyes Detected", (25,35), cv2.FONT_HERSHEY_SIMPLEX, .5, (255, 0, 0), 2)
+							else:
+								cv2.putText(image, "Eyes Open", (25,35), cv2.FONT_HERSHEY_SIMPLEX, .5, (255, 255, 255), 2)
+						else: #eyes not found
+							if time_diff >= capture_time: #Will trigger only if 5s has been reached
+								cv2.putText(image, "Long Blink Detected! (Eyes closed at " + str(round(time_diff,2)) + "s)", (25,35), cv2.FONT_HERSHEY_SIMPLEX, .5, (0, 0, 0), 2)
+								blink_frame = image #Make a copy of the frame at blink capture so that it won't move with the continous camera feeds
+								start = time.time() #Reset time to current time
+								blink_counter = blink_counter + 1
+								print("Blink detected!")
+								self.state = blink_counter
+								cv2.putText(image, "Long Blinks Detected: " + str(blink_counter), (440,70), cv2.FONT_HERSHEY_SIMPLEX, .5, (0, 0, 0), 2)
+								cv2.putText(image, time.ctime(end), (10,460), cv2.FONT_HERSHEY_SIMPLEX, .5, (0, 0, 0), 2)									#print("Image saved: ", cv2.imwrite(str(time.ctime(end).replace(":","").replace(" ",""))+".jpg",blink_frame))
+								#cv2.imshow("Captured Blink", blink_frame) #Show captured frame
 			else:
 				cv2.putText(image, "No Face Detected.", (20,35), cv2.FONT_HERSHEY_SIMPLEX, .5, (0, 255, 255), 2)
 			#cv2.putText(image, "Instructions: q/Q - Quit, s/S Start Blink Capture, r/R - Reset Blink Counter", (10,425), cv2.FONT_HERSHEY_SIMPLEX, .5, (0, 0, 0), 2)
@@ -109,4 +115,3 @@ class detect:
 	#Function for release and destroy all windows set as stop_eye()
 #state = eye()
 #print("State: ", state)
-#detect().run()
